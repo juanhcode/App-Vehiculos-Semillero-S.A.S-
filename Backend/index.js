94834758aa258b01433ebf9d86ef241080344fd4
Juan Hoyos <juanhoyos1347@gmail.com>
2022-04-17 23:55:16 -0500

    routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const morgan = require('morgan');
 
+//Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+app.get('',(req, res) => {
+    res.send('<h1>Hola gente GG</h1>')
+})
+
+//Rutas
+app.use('/vehiculo/',require('./routes/vehiculo'))
+app.use('/linea/',require('./routes/linea'));
+app.use('/marca/',require('./routes/marca'));
+app.use('/consulta/',require('./routes/consulta'))
+
+//PUERTO
+app.set('port',process.env.PORT || 4000)
 app.listen(process.env.PORT,()=>{
-    console.log("Corriendo");
+    console.log("Corriendo en el puerto "+ app.get('port'));
 })
